923ff3af9e45826efaae05c1cfd57f8c1fd21548
Pavel <pashaslovev.@mail.com>
2023-07-23 15:59:15 +1000

    example05

---
+++ example05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "Masha")
+{
+    Console.WriteLine("Ura,ato ge Masha");
+}
+else
+{
+    Console.Write("Привет");
+    Console.Write(username);
+}
